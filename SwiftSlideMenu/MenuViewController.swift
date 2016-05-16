d79e518577576e41a2e62a3d378f36f818b76818
saumya <2saumyaray@gmail.com>
2016-05-16 19:18:09 +0530

    menu open-close is done

--- SwiftSlideMenu/MenuViewController.swift
+++ SwiftSlideMenu/MenuViewController.swift
@@ -9,8 +9,29 @@
 import UIKit
 
 class MenuViewController : UIViewController {
+    // 1
+    var interactor:Interactor? = nil
+    // 2
+    @IBAction func handleGesture(sender: UIPanGestureRecognizer) {
+        // 3
+        let translation = sender.translationInView(view)
+        // 4
+        let progress = MenuHelper.calculateProgress(
+            translation,
+            viewBounds: view.bounds,
+            direction: .Left
+        )
+        // 5
+        MenuHelper.mapGestureStateToInteractor(
+            sender.state,
+            progress: progress,
+            interactor: interactor){
+                // 6
+                self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
     
-    @IBAction func closeMenu(sender:AnyObject){
-        self.dismissViewControllerAnimated(true, completion: nil)
+    @IBAction func closeMenu(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
